abba2439e9742713ce58e379d24c006961c253f8
Aimer Neige <aimer.neige.soft@gmail.com>
2022-06-07 09:02:03 +0800

    fix

--- download_inkscape.sh
+++ download_inkscape.sh
@@ -1,5 +1,5 @@
 set -e
-rm ./bin.inkscape
+rm ./bin/inkscape
 # get the latest release here: https://inkscape.org/release/
 wget https://inkscape.org/gallery/item/33450/Inkscape-dc2aeda-x86_64.AppImage -O ./bin/inkscape
 chmod +x ./bin/inkscape
