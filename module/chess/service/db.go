84ac0ca7906d3a40c74477c87a05aab99be948fa
Aimer Neige <aimer.neige.soft@gmail.com>
2022-08-02 22:00:01 +0800

    update database service

---
+++ module/chess/service/db.go
@@ -0,0 +1,57 @@
+package service
+
+import (
+	"github.com/aimerneige/MiraiChess/module/chess/database/model"
+	"gorm.io/gorm"
+)
+
+// DBService 数据库服务
+type DBService struct {
+	db *gorm.DB
+}
+
+// NewDBService 创建数据库服务
+func NewDBService(db *gorm.DB) *DBService {
+	return &DBService{
+		db: db,
+	}
+}
+
+// CreateELO 创建 ELO
+func (s *DBService) CreateELO(uin int64, name string, rate uint) error {
+	return s.db.Create(&model.ELO{
+		Uin:  uin,
+		Name: name,
+		Rate: rate,
+	}).Error
+}
+
+// GetELOByUin 获取 ELO
+func (s *DBService) GetELOByUin(uin int64) (model.ELO, error) {
+	var elo model.ELO
+	err := s.db.Where("uin = ?", uin).First(&elo).Error
+	return elo, err
+}
+
+// GetELORateByUin 获取 ELO 等级分
+func (s *DBService) GetELORateByUin(uin int64) (uint, error) {
+	var elo model.ELO
+	err := s.db.Select("rate").Where("uin = ?", uin).First(&elo).Error
+	return elo.Rate, err
+}
+
+// UpdateELOByUin 更新 ELO 等级分
+func (s *DBService) UpdateELOByUin(uin int64, name string, rate uint) error {
+	return s.db.Model(&model.ELO{}).Where("uin = ?", uin).Update("name", name).Update("rate", rate).Error
+}
+
+// CreatePGN 创建 PGN
+func (s *DBService) CreatePGN(data string, whiteUin int64, blackUin int64, whiteName string, blackName string) error {
+	return s.db.Create(&model.PGN{
+		Data:      data,
+		WhiteUin:  whiteUin,
+		BlackUin:  blackUin,
+		WhiteName: whiteName,
+		BlackName: blackName,
+	}).Error
+}
